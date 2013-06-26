7e12c34cb6074c15added531436bbc9495ae0956
Peter Lejeck <peter.lejeck@gmail.com>
2013-06-26 14:04:38 -0700

    Make :fg and :bg optional

--- lib/palettetown/rule.rb
+++ lib/palettetown/rule.rb
@@ -10,12 +10,12 @@ module PaletteTown
       # TODO: Need to find nearest console color
       # TODO: Create PaletteTown::TermColor class
       @keys = {}
-      @keys[:guifg] = PaletteTown::Color.new(rule[:fg])
-      @keys[:ctermfg] = rule[:fg_term]
-      @keys[:guibg] = PaletteTown::Color.new(rule[:bg])
-      @keys[:ctermbg] = rule[:bg_term]
-      @keys[:gui] = rule[:style]
-      @keys[:cterm] = rule[:style_term]
+      @keys[:guifg] = PaletteTown::Color.new(rule[:fg]) if rule[:fg]
+      @keys[:ctermfg] = rule[:fg_term] if rule[:fg_term]
+      @keys[:guibg] = PaletteTown::Color.new(rule[:bg]) if rule[:bg]
+      @keys[:ctermbg] = rule[:bg_term] if rule[:bg_term]
+      @keys[:gui] = rule[:style] if rule[:style]
+      @keys[:cterm] = rule[:style_term] if rule[:style_term]
     end
   end
 end
