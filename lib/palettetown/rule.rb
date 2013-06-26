0c3a0a41decd3100638db65bcaae6d1205ab3357
Peter Lejeck <peter.lejeck@gmail.com>
2013-06-26 11:00:10 -0700

    Change style of code

--- lib/palettetown/rule.rb
+++ lib/palettetown/rule.rb
@@ -1,19 +1,21 @@
-class PaletteTown::Rule
-  def [] key
-    @keys[key]
-  end
-  def each &block
-    @keys.each(&block)
-  end
-  def initialize rule
-    # TODO: Need to find nearest console color
-    # TODO: Create PaletteTown::TermColor class
-    @keys = {}
-    @keys[:guifg] = PaletteTown::Color.new(rule[:fg])
-    @keys[:ctermfg] = rule[:fg_term]
-    @keys[:guibg] = PaletteTown::Color.new(rule[:bg])
-    @keys[:ctermbg] = rule[:bg_term]
-    @keys[:gui] = rule[:style]
-    @keys[:cterm] = rule[:style_term]
+module PaletteTown
+  class Rule
+    def [] key
+      @keys[key]
+    end
+    def each &block
+      @keys.each(&block)
+    end
+    def initialize rule
+      # TODO: Need to find nearest console color
+      # TODO: Create PaletteTown::TermColor class
+      @keys = {}
+      @keys[:guifg] = PaletteTown::Color.new(rule[:fg])
+      @keys[:ctermfg] = rule[:fg_term]
+      @keys[:guibg] = PaletteTown::Color.new(rule[:bg])
+      @keys[:ctermbg] = rule[:bg_term]
+      @keys[:gui] = rule[:style]
+      @keys[:cterm] = rule[:style_term]
+    end
   end
 end
